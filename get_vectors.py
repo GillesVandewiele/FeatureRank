4c993280b88744ea52e5ae8e3ec744d0ae15716f
GillesVandewiele <givdwiel.vandewiele@ugent.be>
2017-06-07 13:44:02 +0200

    more animals

--- get_vectors.py
+++ get_vectors.py
@@ -7,7 +7,10 @@ model = gensim.models.Word2Vec.load('db2vec_sg_200_5_25_5')
 
 tag_dict = []
 for _class, tag in zip(df['class'], df['rdf2vec_tag']):
-	tag_dict.append({'class': _class, 'tag': tag, 'vector': model.wv[tag]})
+	try:
+		tag_dict.append({'class': _class, 'tag': tag, 'vector': list(model.wv[tag])})
+	except:
+		print(tag, 'failed')
 
 output = pd.DataFrame.from_dict(tag_dict)
 output.to_csv('vectors.csv')
